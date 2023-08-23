cbf7c27664aa67d362ffc9cb50b76e83b4fbfa32
Belen Gotz <belugotz@gmail.com>
2023-08-23 14:06:20 -0300

    tp1

---
+++ ejercicios/TP/trabajo_practico1.py
@@ -0,0 +1,5 @@
+from termcolor import colored, cprint
+cansados = colored(9*'?', 'dark_grey')
+dormidos = colored(5*'?', 'cyan')
+despiertos = colored('?', 'light_red')
+print(f'2 {cansados}{dormidos}{despiertos}')
