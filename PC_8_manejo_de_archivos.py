37fa75ce7982fbe6bdb9b7f2f779fcbe82c4c5b5
Belen Gotz <belugotz@gmail.com>
2023-09-11 09:26:21 -0300

    reorganizacion

---
+++ PC_8_manejo_de_archivos.py
@@ -0,0 +1,11 @@
+# 11/9
+
+# CODIGO ASCII
+# UNICODE --> incluye TODOS los caracteres
+ord() #-->convierte valor a unicode
+chr() #-->viceversa
+
+# ARCHIVOS: secuencia de datos almacenados en la memoria
+    # TEXTO PLANO: SOLO caracteres unicode (sin info extra, solo 1 y 0)
+        # ej, txt, .py, CSV (para agrupar datos)
+    # BINARIOS: cualquier cosa guardada en la compu con 0 y 1
