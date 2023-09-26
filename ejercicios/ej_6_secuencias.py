df2a7cc572f4031a7c374c83e8347e25e9275be5
Belen Gotz <belugotz@gmail.com>
2023-09-26 16:39:52 -0300

    ejercicios archivos

--- ejercicios/ej_6_secuencias.py
+++ ejercicios/ej_6_secuencias.py
@@ -103,7 +103,20 @@ joyas2 = 'z'
 print(joyas(Piedras1,joyas1))
 print(joyas(Piedras2, joyas2))
 
-#%% ejercicio 7
+
+
+
+
+
+
+#%% EJERCICIOS CLASE DE PROBLEMAS ejercicio 7
+'''Queremos acortar el largo de un mensaje para que entre 
+en una cantidad fija de caracteres, n. Si el mensaje tiene más de n 
+caracteres, es decir, no entra en n caracteres, entonces eliminaremos
+el excedente y lo completaremos con ?.? hasta completar los n 
+caracteres, y a lo sumo 3 puntos. En la tabla siguiente se muestran,
+dados un mensaje y un n, 
+se muestran ejemplos del resultado esperado.'''
 texto = "algo de texto para probar"
 largo_impresion = 5
 largo_texto = len(texto)
@@ -112,7 +125,14 @@ if largo_impresion < 4:
 elif largo_texto > largo_impresion:
     print(f'{texto[:largo_impresion-2:]}...')
 else: print(texto)
-# %%
+
+
+
+
+
+
+
+# %% EJERCICIOS CLASE DE PROBLEMAS
 '''Realizar una funcion que dad una lista de enteros,
 devuelva tres listas:
 1. La misma lista en reversa
@@ -126,14 +146,14 @@ def f_listas(lista_a):
     return b,c,d
 
 print(*f_listas([4,5,6,8,2,4,2,99])) # el * al principio desempaqueta todo
-# %%
+# %% EJERCICIOS CLASE DE PROBLEMAS
 '''Realizar una funcion que dad una lista de numeros,
 devuelva la suma de los elementos positivos y la suma de los elementos negativos'''
 def sumas(numeros:list)-> tuple:
     pos = sum([num for num in numeros if num >=0])
     neg = sum([num for num in numeros if num <0])
     return pos, neg
-#%% ejercicio 12
+#%% ejercicio 12 EJERCICIOS CLASE DE PROBLEMAS
 '''Implementar una funcion que dadas 2 secuencias recibidas como argumentos, retorne los indices de cada una de ellas
 a partir de los cuales se obtiene la subsecuencia en comun de mayor longitud, 
 y retorne tmb la longitus de dicha secuencia'''
@@ -153,20 +173,20 @@ seq1 = ""
 seq2 = ""
 print(alineacion(seq1,seq2))
 
-#%%
+#%% EJERCICIOS CLASE DE PROBLEMAS
 lis = ["6", "31"] 
 lista = []
 for i in (lis):
     lista += [int(i)]
 lista
-# %%
+# %% EJERCICIOS CLASE DE PROBLEMAS
 '''Imprima en la consola de Python interactivo los números pares del 0 al 100'''
 counter = 0
 while counter<100:
     counter += 1
     print(counter)
 
-# %%
+# %% EJERCICIOS CLASE DE PROBLEMAS
 '''Escriba un programa que imprima en pantalla los números que son 
 a la vez múltiplos de 7 y 23 (por ejemplo, 161 es múltiplo de ambos, 
 21 es múltiplo de 7 pero no de 23 y 46 es múltiplo de 23 pero no de 7).'''
@@ -174,7 +194,7 @@ num = int(input("num: "))
 for i in range (num):
     if i%7==0 and i%23==0:
         print(i)
-#%%
+#%% EJERCICIOS CLASE DE PROBLEMAS
 '''Escribir un programa que pida al usuario dos números A y B e 
 imprima en la pantalla todos los números múltiplos de, a la vez, 7 y 23, 
 en el intervalo [A, B).'''
@@ -187,7 +207,7 @@ for i in range (A, B):
     if i%7==0 and i%23==0:
         print(i)
 
-#%%
+#%% EJERCICIOS CLASE DE PROBLEMAS
 '''Escribir un programa que pida al usuario cuatro números A, B, C, y D 
 e imprima en la pantalla todos los números múltiplos de, a la vez, C y D, 
 en el intervalo [A, B).'''
@@ -200,7 +220,7 @@ if A > B:
 for i in range (A, B):
     if i%C==0 and i%D==0:
         print(i)
-#%%
+#%% EJERCICIOS CLASE DE PROBLEMAS
 '''Escriba un programa que imprima el producto entre dos números 
 haciendo una suma repetida (sin utilizar el operador de multiplicación * 
 del lenguaje de programación).'''
@@ -210,7 +230,7 @@ a1 = 0
 for i in range(1,B+1):
     a1 += A
 a1 
-# %%
+# %% EJERCICIOS CLASE DE PROBLEMAS
 '''La suma de Gauss permite hallar el resultado de sumar los números naturales entre 1 y n, 
 es decir: 1 + 2 + ? + (n - 1) + n. 
 Escriba un programa que compute esta suma (iterando) y que tenga como entrada el número n. 
@@ -222,7 +242,7 @@ a1 = 0 #!!!!!!!!!!!!!!!!!
 for i in range(0,B+1):
     A += 1
 
-# %%
+# %% EJERCICIOS CLASE DE PROBLEMAS
 #ejercicios clase secuencias 6/9
 '''A partir de la siguiente lista, extraiga las siguientes listas aplicando slicing'''
 lista = [2,4,6,8,10,12,14,16,18]
@@ -230,7 +250,7 @@ lista1 = lista[:4]
 lista2 = lista[6::]
 lista3 = lista[1::3]
 lista4 = lista [::-2]
-# %%
+# %% EJERCICIOS CLASE DE PROBLEMAS
 #ejercicios clase secuencias 6/9
 cadena = input("cadena: ")
 lista = []
@@ -245,7 +265,7 @@ def MAYUSC(cadena):
         lista.append(x)
     return ''.join(lista)
 MAYUSC(cadena)
-# %%
+# %% EJERCICIOS CLASE DE PROBLEMAS
 #ejercicios clase secuencias 6/9
 '''escriba una funcion q reciba una oracion ingresada por el usuario y devuelva una lista (q haga lo del split)'''
 oracion = input("oracion: ")
--- ejercicios/ej_8_archivos/ej_8_archivos.py
+++ ejercicios/ej_8_archivos/ej_8_archivos.py
@@ -1,10 +1,11 @@
+import math
 #%% 1
 '''Escriba una función, llamada less, que reciba un archivo e imprima su contenido.'''
 def less(archivo:str): 
     with open (archivo, 'r') as f:
        return f.read()
 
-print(less('archivo.txt'))
+print(less('dic.txt'))
 # %% 2
 '''Escriba una función, llamada head, 
 que reciba un archivo y un número N e imprima las primeras N líneas.'''
@@ -66,7 +67,7 @@ wc('archivo.prueba.txt')
 e imprima las líneas del archivo que contienen esa cadena.'''
 def grep(cadena:str, archivo:str)->str:
     contenido = open(archivo).read().split()
-    return [linea for linea in contenido if cadena in linea]
+    return [linea for linea in contenido if cadena in linea] #POR COMPRENSION
 
 grep('s', 'archivo.prueba.txt')
 
@@ -75,11 +76,12 @@ grep('s', 'archivo.prueba.txt')
 a los nombres de dos archivos, y guarde en un tercer archivo 
 el contenido de los dos archivos, concatenado.'''
 
-def cat(nombre1:str, nombre2:str):
-    archivo1 = open (nombre1).read()
+def cat(nombre1:str, nombre2:str)->None: #typeskin!!!
+    archivo1 = open(nombre1).read()
     archivo2 = open(nombre2).read()
     archivo_final = open('archivo_final','w')
     archivo_final.write(archivo1+'\n'+archivo2)
+
 
 
 cat('archivo.txt', 'archivo.prueba.txt')
@@ -95,15 +97,12 @@ para obtener un nuevo caracter.'''
 
 def rot123(origen:str, destino:str):
     archivo_origen = open(origen).read().split()
-    archivo_destino = open(destino, 'w')
+    archivo_destino = open(destino, 'w') #ACLARAR W PARA ESCRIBIR
     for palabra in archivo_origen:
         archivo_destino.write(' '.join([chr(ord(caracter)+13) for caracter in palabra \
                                if caracter.isalpha()]))
 
 rot123('archivo_final', 'archivo.txt')
-# %%
-for letra in 'u | y n { |q |   r p  n   |p v { p | r v ':
-    print(chr(ord(letra)-13))
 
 # %% 10
 '''Escribir una función load_data que reciba un nombre de archivo, 
@@ -112,12 +111,126 @@ y devuelva un diccionario con el primer campo como clave y el segundo como dicci
 
 def load_data(nombre:str):
     ''' '''
+    archivo = open(nombre)
+    dic = {}
+    for linea in archivo:
+        elementos = linea.split(':')
+        dic[elementos[0]] = elementos[1] #agregar a diccionario
+    archivo.close()
+    return dic
 load_data('diccionario.txt')
+
+# %% 11
+'''Escribir una función save_data que reciba un diccionario y un nombre de archivo, 
+y guarde el contenido del diccionario en el archivo, con el formato key:value.'''
+def save_data(dic:dict, nombre:str)->None:
+    ''' '''
+    with open(nombre, 'w') as f:
+        f.write(str(dic)) #lo de adentro del write tiene que ser STR!!!
+diccionario = {'hola':1,
+               2: "hola",
+               'chau':3}
+save_data(diccionario, 'dic.txt')
+
+#%% 12
+'''Escribir una función, llamada sed, que reciba el nombre de un archivo, y dos cadenas. 
+La primera cadena en el archivo es la que queremos reemplazar por la segunda.'''
+#??????? que reciba una cadena y 
+def sed(archivo:str, cadena1:str, cadena2:str): #cadena es string???
+    ''' '''
+
+    Lectura = open(archivo) #solo para leer
+    if str(cadena1) in Lectura.read(): #STR!!! SI BUSCO QUE ALGO ADENTRO DEL ARCHIVO 
+        Lectura.close()
+        Archivo = open(archivo, 'w') #xq aca quiero que escriba desde el principio borrando todo, habiendolo leido antes
+        Archivo.write(str(cadena2))
+        Archivo.close()
+
+diccionario = {'hola':1,
+               2: "hola",
+               'chau':3}
+sed('dic.txt', diccionario, 'holahola')        
+    
+#%% 13
+'''Un archivo CSV (Comma Separated Value) es un archivo de texto que, en cada línea,
+almacena un registro. Cada registro está compuesto por campos. 
+En el archivo, cada campo está separado por comas (,), de ahí el nombre. 
+El siguiente texto es un ejemplo del contenido de un archivo con el mencionado formato:
+
+First Name,Last Name,Country,Year of Birth
+Emanuel,Ginobili,Argentina,1977
+Donald,Trump,USA,1946
+Carl,Gauss,Germany,1855
+
+Escribir una función que reciba la información que se quiere escribir en un archivo en formato
+CSV. Esta información tiene que estar estructurada en una secuencia de secuencias.
+Ejemplo:
+> info = [
+  ["First Name","Last Name","Country","Year of Birth"],
+  ["Emanuel","Ginobili","Argentina","1977"],
+  ["Donald","Trump","USA","1946"],
+  ["Carl","Gauss","Germany","1855"]
+]'''
+
+def formato_csv(*categorias:list)->None: #  VER DIFERENCIA ENTRE * Y **
+    ''' '''         #*algo devuelve tuplas (**diccionarios)
+    with open('archivonuevo.csv','x') as f:
+        for lista in list(categorias):
+            #for elemento in lista:
+                #general.append(str(elemento))
+                f.write(f"{','.join(lista)}\n") #OJO COMO FORMULAR XQ EL CURSOR NO HACE ENTER SOLO
+            
+formato_csv(['categoria 1', 'ganador'],['1','2'])
+
+#%% 14
+'''Un archivo TSV (Tab Separated Value) es un archivo, similar al CSV, 
+que está escrito del siguiente modo:
+    First Name\tLast Name\tCountry\Year of Birth
+    Lewis\tHamilton\tUnited Kingdom\t1985
+    Max\tVerstappen\tBelgium\t1997
+    Charles\tLeclerc\tMonaco\t1997
+Donde cada espacio es un caracter tabular ('\t'); 
+en ASCII es el código 09 (distinto al caracter espacio).
+
+Escribir una función que reciba la información que se quiere escribir en un archivo 
+en formato TSV. Esta información tiene que estar estructurada en una secuencia de secuencias.
+'''
+
+def funcion_tsv(categorias:list)->None:
+    with open('archivonuevo.tsv','x') as f:
+        for lista in categorias:
+            f.write(f"{','.join(lista)}\n") #transformarlo en lista!!!
+
+funcion_tsv([
+  ["First Name","Last Name","Country","Date of Birth"],
+  ["Lewis","Hamilton","United Kingdom","1985"],
+  ["Max","Verstappen","Belgium","1997"],
+  ["Charles","Leclerc","Monaco","1997"]
+])
+
+#%% 15
+
+'''Dado dos archivos en formato CSV, dinosaurs1.csv y dinosaurs2.csv, 
+escribir un programa que lea los dos archivos guardados en disco, 
+y luego imprima los nombres de los dinosaurios bípedos, 
+desde el más lento al más rápido.'''
+dino1 = open('dinasours1.csv')
+dino2 = open('dinasours2.csv')
+
+
+
+g = 9.8 # gravity constant
+#velocity = ((list(dino2[1]) / list(dino1(1))) - 1) * math.sqrt(list(dino1(1)) * g)
+
+#%% 16
+
+#%% pruebas
+
+def formato_csv(**categorias)->None: #  VER DIFERENCIA ENTRE * Y **
+    ''' '''
+    dic = {}
+    for key,value in categorias.items():#PARENTESIS
+        dic[key] = value
+    return dic
+formato_csv(categoria='juegos', ganador='yo')
 # %%
-'''def load_data(archivo:str):
-    r = open(archivo).read()
-    archivo_nuevo1 = open('archivo_nuevo1','w')
-    archivo_nuevo2 = open('archivo_nuevo2','w')
-    archivo_nuevo1.write(dict(';'.join(r).keys()))
-    archivo_nuevo2.write(dict(';'.join(r)).values())
-load_data('diccionario.txt')'''
