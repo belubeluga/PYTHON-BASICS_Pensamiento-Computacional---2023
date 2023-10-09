4cdb99d80a902e3479a0644f8af071049bf25496
Belen Gotz <belugotz@gmail.com>
2023-10-09 09:59:12 -0300

    apuntes 9/10

---
+++ PC_10_algoritmos_de_busqueda.py
@@ -0,0 +1,50 @@
+#--------------------ALGORITMOS DE BUSQUEDA-------------------- 9/10
+#find index in
+
+#busqueda lineal:
+def buscar(lista:list, target) -> int:
+    """
+    Obtener el menor índice en lista donde se encuentre target.
+    Argumentos:
+    lista (secuencia) -- secuencia donde buscar target
+    target -- elemento a buscar en la lista
+    Retorna -1 en caso de no encontrar el elemento.
+    """
+    for i in range(len(lista)):
+        if lista[i] == target:
+            return i
+    return -1 
+
+#busqueda lineal con corte:
+        #(si esta ordenado)
+'''Para todo i entre 0 y N (no inclusive): 
+    si lista[i] es igual a target entonces:
+        devolver i
+    si lista[i] es mayor a target entonces: 
+        devolver -1 
+    devolver -1'''
+#(si el el elemento es mayor al target y todavia no lo encontro, se corta) xq todos los que siguen tmb van a ser mayores
+#NO busco hasta el final
+
+#busqueda binaria
+    #ir subdividiendo aprox a la mitad
+'''izquierda <-- 0
+    derecha <-- N - 1
+    mientras izquierda sea menor o igual a derecha:
+        medio <-- (izquierda + derecha)/2 (redondeado para abajo) 
+        si lista[medio] es igual a target:
+            devolver medio
+        si target es mayor a lista[medio]:
+            izquierda <-- medio + 1 
+        sino:
+            derecha <-- medio - 1 
+    devolver -1'''
+# defino dos variables (principio y final)
+# busco medio (y redondeo para abajo)
+    # si medio es igual a target, devuelvo medio
+    # si no, redefino limites y vuelvo a calcular medio
+        # si target > medio: izquierda = medio + 1
+        # si target < medio: derecha = medio -1
+
+
+        
