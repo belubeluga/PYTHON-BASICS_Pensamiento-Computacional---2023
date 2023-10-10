2b7f46acff55f76395955a5d51398342307e2d5e
Belen Gotz <belugotz@gmail.com>
2023-10-10 11:25:26 -0300

    updates

--- PC_10_algoritmos.py
+++ PC_10_algoritmos.py
@@ -59,7 +59,11 @@ def buscar(lista:list, target) -> int:
 #notacion Big-O 
     #costo temporal de nuestro algoritmo T(N)
 
+'''conviene usarlo cuando vamos a hacer muchas busquedas (xq solo se ordena una vez)'''
+#mientras mas busquedas, se diluye el costo del ordenamiento
+
 #PROPIEDADES: se suma el Big-O del peor caso
+#sumatoria de la cantidad de iteraciones
 
 '''     Orden         Nombre
         -----------------------
@@ -72,17 +76,102 @@ def buscar(lista:list, target) -> int:
         O(an)      |  exponencial
         O(n!)      |  factorial      '''
 
-#VER EJEMPLOS!!!!!
-n = input()
+#VER EJEMPLOS!!!!! y como calcularlo matematicamente
+'''n = input()
 j=0
-
+#a)
 if n%2==0:      #complejidad = O(1)
     for i in range(n):
         print('par')
     else:
         print('impar')
-
+#b)
 for i in range(n):  #complejidad = O(n)
     if i%2==0:
         j += 2
     print(i,j)
+#c)
+for i in range(n):
+    for j in range(n):  #complejidad = O(n**2)
+        if i%2==0:
+            print(i)
+            print(j)
+print(i+j)
+#d)
+for i in range(n):
+    if i%2==0:      #complejidad = O(n**2)
+        print(i)
+for i in range(n):
+    for k in range(n):
+        print(k)
+#e)
+i=1
+while i<n:   #complejidad = O(log(n))
+    i*=2
+    print(i)
+#f)
+for i in range(n):
+    j=1
+    while j<n:  #complejidad = O(n*log(n))
+        j*=2
+        print(j)'''
+#%%
+lista = [5,6,2,4,1,3] 
+n = len(lista)
+lista_nueva = []   
+#ORDENAMIENTO POR SELECCION
+for j in range(0,n): #(n-2 incluido) xq el ultimo no lo ordeno 
+    #for k in range(j,n):#(n-1 incluido) k indica la sublista ->  complejidad =O(n**2) (xq depende del anterior)
+        #chequear y guardar MIN de sublista
+        #intercambiar minimo con inicio de lista sin ordenar -> complejidad = O(1)
+    minimo = min(lista)
+    lista_nueva+=[minimo]
+    lista.remove(minimo)
+#print(lista_nueva)
+
+#ORDENAMIENTO POR BURBUJEO
+
+
+# %% Problema 1
+'''Generar una lista de números aleatorios. 
+Luego, implementar el ordenamiento de la lista mediante una función que ordene por burbujeo. 
+Ejemplo:
+   lista = [2, 1, 5, 6, -4, 0]
+   lista_ordenada = burbujeo(lista)
+   print(lista)
+   print(lista_ordenada)'''
+lista = [7,6,9,8,4,5,2,1,0,-2,-8]
+
+def burbujeo(lista):
+    '''     '''
+    n=len(lista)
+    for j in range(1,n):
+        for k in range(0,j):
+            if lista[j]>=lista[k]:
+               #quedan igual
+               lista
+            else: 
+                lista[j],lista[k] = lista[k],lista[j]
+    return lista                  
+lista_ordenada = burbujeo(lista)
+#print(lista_ordenada)
+# %% Problema 2
+'''Implementar una función busqueda_corte(numero, lista) que reciba una lista ?ordenada? 
+de números enteros y busque en la lista la posición del número pasado por argumento. 
+Si el número no se encuentra en la lista, debe retornar -1. Ejemplo:
+lista = [-4, 0, 1, 2, 5, 6]
+indice_numero = busqueda_corte(5, lista)
+print(indice_numero)
+indice_numero = busqueda_corte(8, lista)
+print(indice_numero)'''
+def busqueda_corte(numero,lista):
+    '''     '''
+    if numero in lista:
+        return lista.index(numero)
+    else: return -1
+lista = [-4, 0, 1, 2, 5, 6]
+indice_numero = busqueda_corte(5, lista)
+#print(indice_numero)
+indice_numero = busqueda_corte(8, lista)
+#print(indice_numero)
+
--- TP/TP2/prueba_tp3.py
+++ TP/TP2/prueba_tp3.py
@@ -1,19 +1,40 @@
+import numpy as np
 from PIL import Image
-import numpy as np 
-import matplotlib.pyplot as plt
+#PROBLEMA A RESOLVER: al hacer la convolucion, lo nuevo que nos devuelve no coincide con las dimensiones de las imagenes
 
-#%matplotlib inline
+def convolucion(imagen,kernel):
+    imagen = Image.open(imagen)
+    filak,colk = kernel.shape
+    tamaño_imagen = imagen.shape
+    filas_a_agregar = round((filak - 1)/2)
+    forma1 = (tamaño_imagen[0]+filas_a_agregar,tamaño_imagen[1]+filas_a_agregar)
+    valor = 0
+    array0 = np.zeros(forma1)
+    imagen = np.array(imagen)
+    for i in range(filak):
+        for j in range(colk):
+            for fk in range(filak):
+                for ck in range(colk):
+                    valor += imagen[i+fk][j+ck][ck]*kernel[fk][ck]
+            #valores.append(valor)
+            array0[i+1][j+1]= valor
+            array0[i+2][j+2]= valor
+    array0[1::] = []
+    return array0
 
-def solo_rojos(imagen:str):
-    '''     '''
-    img = imagen.copy()
-    img_np = np.array(img)
-    for i in range(img_np):
-        for j in range(img_np):
-            r = img_np[i][j][0]
-            g = img_np[i][j][1]
-            b = img_np[i][j][2]
-            g = 0
-            b = 0
-    img_nueva = Image.fromarray(img_np.astype('unit8'))
-    return img_np, img_nueva
+
+def main():
+    #imagen=('baboon.png') #con este kernel no funciona
+
+    kernel_sobel_horizonal=np.array([[-1,-2,11],[0,0,0],[1,2,1],[-1,-2,11],[0,0,0],[1,2,1]])
+
+    k2 = kernel_sobel_horizonal[1:-1:]
+    for elemento in range(len(k2)):
+        k2[elemento]=k2[elemento][1:-1:]
+   
+    print(k2)
+    #imagen_final=convolucion(imagen,kernel_sobel_horizonal)
+    #print(imagen_final)
+    
+if __name__=='__main__':
+    main()
