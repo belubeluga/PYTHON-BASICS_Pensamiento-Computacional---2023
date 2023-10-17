8bc1f7554a85acda1ccf18435a3b20f0025a9cf9
Belen Gotz <belugotz@gmail.com>
2023-10-17 09:48:30 -0300

    apuntes

--- PC_11_recursion.py
+++ PC_11_recursion.py
@@ -9,3 +9,13 @@
 #stack frame (cuando la funcion finaliza, desaparece (en orden))    (ultimo en entrar primero en salir)
 
 
+#%% ejemplo
+def mult(a:int,b:int)->int:
+    '''     '''
+    if b==1:
+        return a
+    else:
+        return a + mult(a,b-1)
+    
+mult(3,5)
+# %%
