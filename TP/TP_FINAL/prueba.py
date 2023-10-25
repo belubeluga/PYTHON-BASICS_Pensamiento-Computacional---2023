1e262328bf68d76e942aef4c52e5d38269dca0e3
Belen Gotz <belugotz@gmail.com>
2023-10-25 13:47:44 -0300

    ej ejemplo parcial

--- TP/TP_FINAL/prueba.py
+++ TP/TP_FINAL/prueba.py
@@ -1,6 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+
 fig = plt.figure()
-ax=Axes3D(fig)
+ax = fig.add_subplot(111, projection='3d')
+
+# Now you can use the `ax` object to create your 3D plot.
+
+# For example, let's create a simple 3D scatter plot.
+x = np.random.rand(100)
+y = np.random.rand(100)
+z = np.random.rand(100)
+
+ax.scatter(x, y, z)
+
+# Show the plot
 plt.show()
---
+++ problemas/repaso_parcialito2_enunciado1.py
@@ -0,0 +1,31 @@
+#%% ejercicio 3
+family_tree = {
+    "a": (),
+    "b": (),
+    "c": ("a", "b"),
+    "d": ("a", "b"),
+    "e": ("a", "b"),
+    "f": ("m", "n"),
+    "g": ("f", "c"),
+    "h": ("f", "c"),
+    "i": ("d",),
+    "j": ("d",),
+    "k": (),
+    "l": ("j", "k"),
+    "m": (),
+    "n": (),
+}
+
+def es_ancestro(apellido1,apellido2, arbol):
+    if apellido1 in arbol[apellido2]:
+        return True
+    if not arbol[apellido2]:
+        return False
+    
+    padres = arbol[apellido2]
+    for padre in padres:
+        es = es_ancestro(apellido1, padre, arbol)
+        if es:
+            return True
+    return False
+print(es_ancestro("d","l",family_tree))
