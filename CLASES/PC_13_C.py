2667e58b90920867a6aa7c4852f46d68b2961d0b
Belen Gotz <belugotz@gmail.com>
2023-10-30 09:49:23 -0300

    clase 30/10

--- CLASES/PC_13_C.py
+++ CLASES/PC_13_C.py
@@ -1,2 +1,4 @@
 #-------------------- C -------------------- 30/10
 #codigo compilador ejecutable ejecucion
+'''gcc -Wall -std=c17 -o ejecutable hola.c
+./ejecutable'''
---
+++ hola.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/* ***** MAIN ******
+        Función principal
+        Ejemplo: Hola mundo!
+*/
+int main(void)
+    {
+        printf("\nHola mundo!\n\n"); //imprimir mensaje
+
+        return 0;
+    }
+
+/*  
+
+EJECUTABLE Y EJECUTAR:
+----------------------
+gcc -Wall -std=c17 -o ejecutable hola.c  
+./ejecutable
+
+
+TIPOS DE DATOS
+--------------
+* char -> caracter, 1 byte
+* short
+* int
+* long
+* float 
+* double
+
+*/
+
